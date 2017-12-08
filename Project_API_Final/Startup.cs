6cb59a97e0bba85a9e94691feb732d9b5f0a640d
ZeusAC <fabiociconi@yahoo.com.br>
2017-12-08 14:24:19 -0200

    Swagger

--- Project_API_Final/Startup.cs
+++ Project_API_Final/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Project_API_Final
 {
@@ -24,7 +25,18 @@ namespace Project_API_Final
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-        }
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new Info { Title = "FinalProject API", Version = "v1" });
+				c.AddSecurityDefinition("Bearer", new ApiKeyScheme()
+				{
+					Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\"",
+					Name = "Authorization",
+					In = "header",
+					Type = "apiKey"
+				});
+			});
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -33,8 +45,16 @@ namespace Project_API_Final
             {
                 app.UseDeveloperExceptionPage();
             }
+		
 
-            app.UseMvc();
+            app
+				.UseMvc()
+				.UseSwagger()
+				.UseSwaggerUI(c =>
+				{
+					c.ShowJsonEditor();
+					c.SwaggerEndpoint("/swagger/v1/swagger.json", "FinalProject API");
+				});
         }
     }
 }
